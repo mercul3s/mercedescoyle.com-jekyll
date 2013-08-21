b2691385e58e607db7c182615433f8ec11191133
Mercedes Coyle <mcoyle@realgravity.com>
2013-08-21 08:06:20 -0700

    added mongo connections and writes to db, looking at adding numbered ids for posts

--- comments/post_data.rb
+++ comments/post_data.rb
@@ -1,16 +1,30 @@
 require 'sinatra'
 require 'logger'
+require 'mongo'
+require 'json'
+
+include Mongo
 
 # We'll use the standard ruby logger to log params to a file.
 
-logger = Logger.new("comments.log")
+logger             = Logger.new("comments.log")
 original_formatter = Logger::Formatter.new
+
+mongo_client = MongoClient.new("localhost", 27017)
+db           = mongo_client.db("mercedescoyle-site")
+collection   = db.collection("comments")
 # logger.formatter = proc { |severity, datetime, progname, msg|
 #   original_formatter.call(severity, datetime, progname, msg.dump)
 # }
 
 # for now, display output and log to file.
+# make sure all params are in json form
+# also make sure to validate the params
+# to make sure they are valid and safe
+
 get '/comment' do
   logger.info("#{params}")
+  doc = params
+  collection.insert(params)
   "#Hello World! #{params}"
 end
